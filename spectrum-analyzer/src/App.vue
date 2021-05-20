148612a028da3ca3a5ccd122f9246dba9f7a4d43
Sean <seandvir12@intuit.com>
2021-05-20 15:31:26 +0300

    spectrum analyzer

---
+++ spectrum-analyzer/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <SpectrumVisualizer/>
+</template>
+
+<script setup>
+import SpectrumVisualizer from './components/SpectrumVisualizer.vue'
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ spectrum-analyzer/src/components/SpectrumVisualizer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <h1>
+      Spectrum Visualizer
+    </h1>
+    <button type="button" class="btn btn-primary mb-3" @click="toggleAudio">
+      {{isPlaying ? 'Stop' : 'Play'}}
+    </button>
+    <div class="d-flex justify-content-center" ref="canvas"></div>
+  </div>
+</template>
+
+<script>
+import { defineProps, reactive } from 'vue'
+import p5 from 'p5';
+import setup, { toggle } from '../analyze'
+
+export default {
+  name: 'p5Container',
+  mounted() {
+    new p5(setup, this.$refs.canvas)
+  },
+  data() {
+    return {
+      isPlaying: false,
+    }
+  },
+  methods: {
+    toggleAudio() {
+      this.isPlaying = !this.isPlaying;
+      toggle();
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
